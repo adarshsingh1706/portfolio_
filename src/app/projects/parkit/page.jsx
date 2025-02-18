412e090774e6e703797a7b26222e7cab15594691
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2025-02-19 00:13:47 +0530

    added layout grid with images related to parkit

--- src/app/projects/parkit/page.jsx
+++ src/app/projects/parkit/page.jsx
@@ -1,5 +1,9 @@
+"use client";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import React, { useState, useRef, useEffect } from "react";
+import { LayoutGrid } from "@/components/ui/layout-grid";
 
+/* for hero section */
 const words = [
   {
     text: "Park ",
@@ -18,6 +22,99 @@ const words = [
     className: "text-blue-500 dark:text-blue-500",
   },
 ];
+
+
+/* for layout grid */
+const SkeletonOne = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        House in the woods
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A serene and tranquil retreat, this house in the woods offers a peaceful
+        escape from the hustle and bustle of city life.
+      </p>
+    </div>
+  );
+};
+ 
+const SkeletonTwo = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        House above the clouds
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        Perched high above the world, this house offers breathtaking views and a
+        unique living experience. It&apos;s a place where the sky meets home,
+        and tranquility is a way of life.
+      </p>
+    </div>
+  );
+};
+const SkeletonThree = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        Greens all over
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
+      </p>
+    </div>
+  );
+};
+const SkeletonFour = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        Rivers are serene
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A house by the river is a place of peace and tranquility. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
+      </p>
+    </div>
+  );
+};
+ 
+const cards = [
+  {
+    id: 1,
+    content: <SkeletonOne />,
+    className: "md:col-span-2 h-[280px]",
+    thumbnail:
+      "/layout1.jpg",
+  },
+  {
+    id: 2,
+    content: <SkeletonTwo />,
+    className: "col-span-1 h-[280px]",
+    thumbnail:
+      "/layout2.jpg",
+  },
+  {
+    id: 3,
+    content: <SkeletonThree />,
+    className: "col-span-1 h-[280px]",
+    thumbnail:
+      "/layout3.jpg",
+  },
+  {
+    id: 4,
+    content: <SkeletonFour />,
+    className: "md:col-span-2 h-[280px]",
+    thumbnail:
+      "/layout4.jpg",
+  },
+];
+
 
 export default function ParkItPage() {
   return (
@@ -50,6 +147,16 @@ export default function ParkItPage() {
           </button>
         </div>
       </div>
+
+{/* Layout Grid */}
+
+<p className="text-center text-lg font-medium text-neutral-600 dark:text-neutral-200 mb-6">
+  Click on a card to explore more details!
+</p>
+
+      <div className="h-screen py-20 w-full">
+      <LayoutGrid cards={cards} />
+    </div>
     </>
   );
 }
